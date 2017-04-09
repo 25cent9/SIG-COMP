20fb4f093945e79c986d4734cb57fa77cc69d737
Innocent Niyibizi <25cent9@gmail.com>
2017-04-09 11:30:45 -0500

    Soylent
    Kattis: ‘Soylent’
    Language: Python 3
    Status: Accepted
    CPU: 0.03s

--- Soylent/main.py
+++ Soylent/main.py
@@ -1,10 +1,10 @@
 #   Innocent Niyibizi
 #   4-2-17
 #   Kattis: 'Soylent'
-
+from math import ceil
 caloriesPerBot = 400
 testCases = int(input())
 
 for test in range(0, testCases):
     calories = int(input())
-    print(int(calories/caloriesPerBot))
+    print(ceil(calories/caloriesPerBot))
