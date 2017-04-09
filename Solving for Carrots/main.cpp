011df44af271a2425c0715fa7d901e93169ff50f
25cent9 <25cent9@gmail.com>
2017-04-08 22:27:00 -0500

    Solving for Carrots
    Kattis: ‘Solving for Carrots’
    Language: C++
    Status: Accepted
    CPU: 0.00s

---
+++ Solving for Carrots/main.cpp
@@ -0,0 +1,20 @@
+/*
+  Innocent Niyibizi
+  4-8-17
+  Kattis: 'Solving for Carrots'
+*/
+
+#include <iostream>
+
+int main()
+{
+  int N, P;
+  std::string input;
+  std::cin>>N>>P;
+  for(int i = 0; i< N; i++)
+  {
+    std::cin>>input;
+  }
+  std::cout<<P<<std::endl;
+  return 0;
+}
