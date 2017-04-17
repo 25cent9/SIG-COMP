fb4389f4f661da50a6d7a5aedd97a3b97f5f5065
25cent9 <25cent9@gmail.com>
2017-04-16 19:55:57 -0500

    Backspace
    Finally did it with 0.05s

--- Backspace/main.cpp
+++ Backspace/main.cpp
@@ -9,16 +9,17 @@ using namespace std;
 
 int main()
 {
-  string input;
+  string input, output="";
   cin>>input;
   for(int i=0; i<input.length(); i++)
   {
     if(input[i]=='<')
     {
-      input=input.substr(0, i-1)+input.substr(i+1, input.length()-1);
-      i-=2;
+      output.erase(output.length()-1);
     }
+    else
+      output+=input[i];
   }
-  cout<<input<<endl;
+  cout<<output<<endl;
   return 0;
 }
